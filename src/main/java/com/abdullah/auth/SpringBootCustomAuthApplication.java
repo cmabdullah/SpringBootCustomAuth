f76c4784784b5b44c5d35ac8fa1a9f4782f8100f
cmabdullah <cmabdullah21@gmail.com>
2019-11-25 17:59:17 +0600

    initial commit

---
+++ src/main/java/com/abdullah/auth/SpringBootCustomAuthApplication.java
@@ -0,0 +1,13 @@
+package com.abdullah.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdullah/auth/SpringBootCustomAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCustomAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
